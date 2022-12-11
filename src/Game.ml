a69dde869a851cc4d5e4c2219d8921a065bdaf80
Leopold Abignoli <leopold.abignoli@gmail.com>
2022-12-11 11:47:54 +0100

    Ajout des fonctions get_col, get_reg (pour recuperer l'index des registres correspondant à une carte). + fonction move, pour bouger une carte vers une autre location
    Co-authored-by: DanielGilardoni <DanielGilardoni@users.noreply.github.com>

--- src/Game.ml
+++ src/Game.ml
@@ -48,3 +48,111 @@ let initGame gameType cards =
   | Midnight -> initGameAux Midnight 0 cards ((List.init 17 (fun x -> 3)) @ [1])
   | Baker -> initGameAux Baker 0 cards (List.init 13 (fun x -> 4))
   (* | _ -> raise Not_found *)
+
+
+(* Ecriture des fonctions pour la partie I/2, Peut qu'il faudra les mettres ailleurs plus tard *)
+
+exception Empty_Stack
+
+let push stack elt =
+  match stack with
+  | stack -> elt :: stack
+
+let pop = function
+  | [] -> raise Empty_Stack
+  | a :: stack -> (a, stack)
+
+let peek = function
+  | [] -> None
+  | a :: stack -> Some a
+
+let empty = function
+  | [] -> true
+  | _ -> false
+
+(* Recuperer l'index de la colonne qui contient cette carte *)
+let get_col columns card_num =
+  let col_list = FArray.to_list columns in
+  let rec get_col_aux cols index =
+    match cols with
+    | [] -> None
+    | col :: _ when card_num = 0 && (empty col) -> Some index
+    | col :: sub_cols -> match (peek col) with
+                         | Some card when (Card.to_num card) = card_num -> Some index
+                         | _ -> get_col_aux sub_cols (index+1)
+  in get_col_aux col_list 0
+
+let empty_col columns = 
+  get_col columns 0
+
+(* Renvoie l'index de la carte dans les registres *)
+let get_reg registers card_num =
+  let reg_list = FArray.to_list registers in
+  let rec get_reg_aux regs index =
+    match regs with
+    | [] -> None
+    | card :: _ when card_num = 0 && card = None -> Some index
+    | card :: sub_regs -> match card with
+                          | Some card when (Card.to_num card) = card_num-> Some index
+                          | _ -> get_reg_aux sub_regs (index+1)
+  in get_reg_aux reg_list 0
+
+(* Recuperer l'index du premier registre vide *)
+let empty_reg registers =
+  get_reg registers 0
+
+exception No_Register
+exception No_Column
+exception No_Index
+
+let remove_in_col cols card =
+  let index = get_col cols card in
+  match index with
+  | None -> raise No_Index
+  | Some i -> let col = get cols i in
+              match col with
+              | [] -> raise Empty_Stack
+              | _ :: sl -> set cols i sl
+
+let remove_in_reg regs card =
+  let index = get_reg regs card in
+  match index with 
+  | None -> raise No_Index
+  | Some i -> set regs i None
+
+let add_to_reg registers card =
+  let reg = empty_reg registers in 
+  match reg with
+  | None -> raise No_Register
+  | Some index -> set registers index (Some (Card.of_num card))
+
+(* Si card2 = 0, alors get_col renvoie l'index de la premiere colonne vide. *)
+let add_to_col columns card card2 =
+  let index = get_col columns card2 in
+  match index with
+  | None -> raise No_Column
+  | Some i -> let col = get columns i in
+              set columns i ((Card.of_num card) :: col)
+
+(* Verifier si card_num2 vaut bien [1,51] AVANT *)
+let move game card_num location =
+  let card2 = int_of_string(location) in
+  match location with 
+  | "T" -> let reg = add_to_reg game.registers card_num 
+           in {name = game.name; registers = reg; columns = game.columns; depots = game.depots}
+                           
+  | "V" -> let columns = add_to_col game.columns card_num 0
+           in {name = game.name; registers = game.registers; columns = columns; depots = game.depots}
+  
+  | _ when card2 > 0 && card2 < 52 -> let columns = add_to_col game.columns card_num card2
+           in {name = game.name; registers = game.registers; columns = columns; depots = game.depots}
+    
+  | _ -> raise Not_found
+
+  (*
+  
+  - Fonction rules
+  - Normalisation
+  - Résoudre: ça lit un fichier -> liste de lignes -> pour chaque ligne split sur l'espace -> puis rules, remove, move sur mot1 mot2
+  
+  *)
